5ab0bea084f742c14c1e3ff0066be59a3d8bb72f
Richard McSorley <richard.mcsorley@hey.com>
2021-10-29 10:37:40 -0400

    Add a11y testing using axe-core.

--- myorg/apps/todos-e2e/src/integration/app.spec.ts
+++ myorg/apps/todos-e2e/src/integration/app.spec.ts
@@ -1,11 +1,19 @@
-import { getAddTodoButton, getTodos } from '../support/app.po';
+import { getAddTodoButton, getTodos, checkA11y } from '../support/app.po';
 
 describe('TodoApps', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+    cy.injectAxe();
+  });
 
   it('should display todos', () => {
     getTodos().should((t) => expect(t.length).equal(2));
     getAddTodoButton().click();
     getTodos().should((t) => expect(t.length).equal(3));
   });
+
+  it('should be accessible', () => {
+    checkA11y();
+  })
+
 });
